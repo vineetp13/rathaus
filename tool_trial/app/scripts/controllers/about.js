773cb1f980071c66f2f6b84c9497da98070ce585
Vineet Pandey <vipandey@eng.ucsd.edu>
2015-08-14 20:23:21 +0200

    added new dir: tool_trial to develop tool

---
+++ tool_trial/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name toolTrialApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the toolTrialApp
+ */
+angular.module('toolTrialApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ tool_trial/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name toolTrialApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the toolTrialApp
+ */
+angular.module('toolTrialApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
