314ea83f6bd83b09f419bb514752efe1d0857f19
Vineet Pandey <vipandey@eng.ucsd.edu>
2015-08-24 16:00:46 +0200

    debugged: using npm start to bring up same page as 'grunt serve'. Not fixed scss error, but that should be done with a node-sass plugin. Now doing: persisting json to node - and then working on interface

--- tool_trial/app.js
+++ tool_trial/app.js
@@ -5,13 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./routes/index'); //edit by vineet
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app')); //edit by vineet
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -21,9 +21,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'app'))); //edit by vineet
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/styles',  express.static(__dirname + '/styles'));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes); //edit by vineet
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
